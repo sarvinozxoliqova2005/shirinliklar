8dd77fca8fbb32bea88226df5f680054cb99af28
sarvinozxoliqova2005 <seyranseyran120@gmail.com>
2026-06-21 22:34:39 +0500

    mdnf

--- components/ProductCatalog.jsx
+++ components/ProductCatalog.jsx
@@ -95,7 +95,7 @@ const ProductCatalog = () => {
         ))}
       </div>
 
-      <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-8">
+      <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8">
         {products.map((product) => (
           <div
             key={product.id}
